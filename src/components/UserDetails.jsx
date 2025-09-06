b4b57461155fce618890de58b8b0a1e6eb98b523
allyavorsky <allyavorsky@proton.me>
2025-09-06 17:53:11 +0300

    HW44-4: read user data form redux store

--- src/components/UserDetails.jsx
+++ src/components/UserDetails.jsx
@@ -1,19 +1,14 @@
-import { useContext } from "react";
-import { UserContext } from "../context/UserContext";
+import { useSelector } from "react-redux";
 
 const UserDetails = () => {
-  const user = useContext(UserContext);
-
-  if (!user) {
-    return <p>Дані користувача завантажуються...</p>;
-  }
+  const user = useSelector((state) => state.user);
 
   return (
     <div
       style={{ border: "1px solid green", padding: "16px", marginTop: "16px" }}
     >
       <h3>Component UserDetails (Рівень 3)</h3>
-      <p>Компонент отримав дані напряму з Context.</p>
+      <p>Компонент отримав дані напряму з Redux Store.</p>
       <p>Ім'я: {user.name}</p>
       <p>Email: {user.email}</p>
     </div>
