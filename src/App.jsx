df18c99ce7882e4b32862dd58d4b53c2b6a5fce4
allyavorsky <allyavorsky@proton.me>
2025-09-06 17:50:29 +0300

    HW44-3: connect redux store

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,13 @@
 import UserProfile from "./components/UserProfile";
-import { UserContext } from "./context/UserContext.jsx";
 
 function App() {
-  const userData = {
-    name: "Ярослав",
-    email: "student@example.com",
-  };
-
   return (
-    <UserContext.Provider value={userData}>
-      <div style={{ border: "1px solid blue", padding: "16px" }}>
-        <h1>Component App (Рівень 1)</h1>
-        <p>Компонент роздає данні всім через Context.Provider.</p>
+    <div style={{ border: "1px solid blue", padding: "16px" }}>
+      <h1>Component App (Рівень 1)</h1>
+      <p>Компонент не керує станом. Всі дані надходять з Redux.</p>
 
-        <UserProfile />
-      </div>
-    </UserContext.Provider>
+      <UserProfile />
+    </div>
   );
 }
 
